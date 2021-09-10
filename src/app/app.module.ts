47dc438aa9c5e873db3665320d35ab379ae1b481
Dheepanbalaji <dheepanbalajicse@gmail.com>
2021-09-10 20:09:35 +0530

    angular4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
